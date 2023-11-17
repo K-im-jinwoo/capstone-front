eb910f287766198fa4a51c7e6caa1971d0fc8c16
kristin-dongwoo <dong99woo@kristinkorea.com>
2023-11-17 16:02:57 +0900

    [Feat] 예약 항목 선택 페이지 완성(아이콘 제외)

---
+++ pages/Reservation/constant.tsx
@@ -0,0 +1,17 @@
+export const RESERVATIONITEM = [
+  {
+    id: 0,
+    title: "상담",
+    icon: "asd",
+  },
+  {
+    id: 1,
+    title: "학과 기물",
+    icon: "asd",
+  },
+  {
+    id: 2,
+    title: "강의실",
+    icon: "asd",
+  },
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,12 @@
 import React from "react";
 import '../app/globals.css'
-import { Layout } from "@/app/templates/home";
-import Home from "@/app/templates/home/Home";
-import { Button } from "@/app/atoms";
-
-interface LoginResponse {
-    // 로그인 성공 시 반환되는 데이터 타입 정의
-    // 예시로 토큰이 반환된다고 가정합니다.
-    token: string;
-    // 다른 반환 데이터가 있다면 여기에 추가할 수 있습니다.
-  }
-  
-  const handleLogin = async (): Promise<LoginResponse> => {
-    const username = 'user'; // 사용자명
-    const password = 'password'; // 비밀번호
-  
-    try {
-      const response: Response = await fetch('http://3.37.118.103/login/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-      });
-  
-      if (!response.ok) {
-        throw new Error('로그인에 실패했습니다.');
-      }
-  
-      const data: LoginResponse = await response.json();
-      console.log('로그인 성공:', data);
-      // 여기서 토큰 저장 등의 작업을 수행할 수 있습니다.
-      return data; // LoginResponse 형태의 데이터 반환
-    } catch (error) {
-      console.error('로그인 요청 중 에러 발생');
-      throw new Error('로그인에 실패했습니다.');
-    }
-};
+import { Navbar } from "@/app/organisms";
 
 const Main:React.FC = () => {
     return (
-        <Layout>
-            <Home></Home>
-            <Button onClick={handleLogin}></Button>
-        </Layout>
+      <main>
+        <Navbar></Navbar>
+      </main>
     )
   }
 

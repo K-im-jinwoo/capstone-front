201a1a41bcc42b78c4406ab337da4c01fca79fe0
Jinwoo <zaqaz113@naver.com>
2023-12-06 16:57:09 +0900

    [Feat] professors list 가져오기

--- app/templates/About/About.tsx
+++ app/templates/About/About.tsx
@@ -1,19 +1,39 @@
-import { Div } from "@/app/atoms";
-import { Profile } from "@/app/organisms";
 import { FC } from "react";
+import { useState, useEffect } from "react";
+
+import { Div, Button } from "@/app/atoms";
+import { Profile,ProfileProps } from "@/app/organisms";
+import { getServerUrl } from "@/app/utils/getServerUrl";
+import axios from "axios";
 
 export const About: FC = () => {
+  const [professors, setProfessors] = useState<ProfileProps[]>([])
+  const handleProfile = async () => {
+    try {
+      const response = await axios.get(getServerUrl('/professors_list/'))
+      console.log(response.data)
+      setProfessors(response.data)
+    }
+    catch (error){
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    handleProfile();
+  }, []);
   return (
     <Div>
       <Div className="flex justify-center items-center">
-        <Profile name="name" desc="Description"/>
-        <Profile name="name" desc="Description"/>
-        <Profile name="name" desc="Description"/>
-      </Div>
-      <Div className="flex justify-center items-center">
-        <Profile name="name" desc="Description"/>
-        <Profile name="name" desc="Description"/>
-        <Profile name="name" desc="Description"/>
+        {professors.map((professor, index) => (
+          <Profile
+            key={index}
+            name={professor.name}
+            department={professor.department}
+            phone={professor.phone}
+            email={professor.email}
+            lab={professor.lab}
+          />
+        ))}
       </Div>
     </Div>
   );
--- app/utils/getServerUrl.ts
+++ app/utils/getServerUrl.ts
@@ -1,4 +1,4 @@
 export const getServerUrl = (path: string) => {
-    const host = 'http://3.37.118.103'
+    const host = 'http://3.36.8.99'
     return [host, path].join('')
   }

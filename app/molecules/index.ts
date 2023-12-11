33e1a0ca8600ac7d04885385c9c54e41845c588f
Jinwoo <zaqaz113@naver.com>
2023-12-11 19:33:48 +0900

    [fix] rebase

--- app/molecules/index.ts
+++ app/molecules/index.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-export * from './Navigation'
-=======
 export * from './Navigation'
 export * from './ButtonColor'
 export * from './Card'
 export * from './Banner'
 export * from './BIcon'
->>>>>>> develop
--- app/organisms/Navbar.tsx
+++ app/organisms/Navbar.tsx
@@ -1,17 +1,11 @@
 import { FC } from 'react'
 import {Navigation} from '../molecules'
 
-<<<<<<< HEAD
-export const Navbar: FC = () => (
-    <Navigation 
-        title='동의대학교'
-=======
 export const Navbar: FC = () => {
 
     return (
         <Navigation 
         title='Dong-eui University'
->>>>>>> develop
         items={[
             {text:'Home', path:'/'},
             {text:'About us', path:'/About'},

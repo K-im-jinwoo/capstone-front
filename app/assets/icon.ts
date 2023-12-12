602c277bf5ee1b233fa7cc7192dab529b6e0a340
Jinwoo <zaqaz113@naver.com>
2023-12-13 00:58:26 +0900

    [fix] 수정

--- app/assets/icon.ts
+++ app/assets/icon.ts
@@ -2,10 +2,8 @@ import consult from '@/app/assets/icon/consult.png'
 import equipment from '@/app/assets/icon/equipment.png'
 import lectureRoom from '@/app/assets/icon/lectureRoom.png'
 
-const reservation = {
+export const reservation = {
   consult: consult,
   equipment: equipment,
   lectureRoom: lectureRoom,
 }
-
-export default reservation
